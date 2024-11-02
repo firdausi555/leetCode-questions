4eb668ac2d803ba1bdeb5d733b31696b3c2dbf84
Sabil Firdausi <53112395+firdausi555@users.noreply.github.com>
2024-11-02 20:21:33 +0530

    Add files via upload

---
+++ Q2490_LEETCODE.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isCircularSentence(self, sentence) :
+       
+        n = len(sentence)
+        if sentence[0] != sentence[n-1]:
+            return False
+            
+        
+        for i in range(1, n-1):
+            
+            if sentence[i] == ' ':
+                if sentence[i-1] != sentence[i+1]:
+                    return False
+                    
+
+        return True
